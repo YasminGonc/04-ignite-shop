2d5885841930b537506cafc7c3ac7a59c3ba2c72
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-11-11 21:57:00 -0300

    home-style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,31 @@
-import { styled } from "../styles"
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 8,
-  padding: '4px 8px',
-  cursor: 'pointer',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import camiseta1 from '../assets/Variant6.png';
+import camiseta2 from '../assets/Variant7.png';
+import camiseta3 from '../assets/Variant8.png';
 
 export default function Home() {
   return (
-    <Button>
-      <span>
-        Enviar
-      </span>
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="Camiseta 1" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} alt="Camiseta 1" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+    </HomeContainer>
   )
 }
