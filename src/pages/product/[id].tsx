9bf1003e08a80d20a3f05ace7608b6cb57e11695
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-11-21 22:03:30 -0300

    checkout strype

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
+import axios from 'axios';
 import Stripe from 'stripe';
 import { stripe } from '../../lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
@@ -17,14 +19,32 @@ interface ProductProps {
     }
 }
 export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+    
     const { isFallback } = useRouter();
 
     if (isFallback) {
         return <p>Loading...</p>
     }
 
-    function handleBuyProduct() {
-        console.log(product.defaultPriceId);
+    async function handleBuyProduct() {
+        try {
+            setIsCreatingCheckoutSession(true);
+            
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId
+            });
+
+            const { checkoutUrl } = response.data;
+
+            window.location.href = checkoutUrl;
+        }
+        catch (err) {
+            // Conectar com uma ferramenta de observabilidade (Datadog / Sentry)
+            setIsCreatingCheckoutSession(false);
+
+            alert('Falha ao redirecionar ao checkout!');
+        }
     }
 
     return (
@@ -38,7 +58,7 @@ export default function Product({ product }: ProductProps) {
                 <span>{product.price}</span>
                 <p>{product.description}</p>
 
-                <button onClick={handleBuyProduct}>
+                <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
                     Comprar agora
                 </button>
             </ProductDetails>
