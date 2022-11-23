cd1085c4b4dfd631671e37802744e1f1ecf5ee40
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-11-22 22:45:57 -0300

    success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,25 @@
+import Link from 'next/link';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Compra efetuada!</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuul <strong>Yasmin</strong> sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+            </p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer>
     )
 }
+
+// fetch
+// Client-side (useEffect) / getServerSideProps / getStaticProps
